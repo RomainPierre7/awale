5fa2802fbda80266e8d1f47064c4be4cf2bcfe31
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-04-19 00:34:16 +0200

    seed implemented

--- awale.py
+++ awale.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pygame
+import math
 
 # Pygame Initialization
 font_sys = "impact"
@@ -10,11 +10,11 @@ pygame.display.set_caption("Awale")
 screen.fill((150, 70, 30))
 pygame.display.update()
 
-# Game initialization
-board = np.zeros((2, 6), dtype=int)
-board[0] = [4, 4, 4, 4, 4, 4]
-board[1] = [4, 4, 4, 4, 4, 4]
-turn = 0
+# Board initialization
+board = [[4 for _ in range(6)] for _ in range(2)]
+radius = width // 15
+board_loc = [[(width // 2 + (j - 2.5) * radius * 2.5, height // 2 + (i - 0.5) * radius * 2.5) for j in range(6)] for i in range(2)]
+rotation = [[0, 5], [0, 4], [0, 3], [0, 2], [0, 1], [0, 0], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5]]
 
 # Functions
 def update_screen():
@@ -25,9 +25,7 @@ def update_screen():
     screen.blit(text, (width // 2 - len // 2, 10))
     for i in range(2):
         for j in range(6):
-            radius = width // 15
-            center_x = width // 2 + (j - 2.5) * radius * 2.5
-            center_y = height // 2 + (i - 0.5) * radius * 2.5
+            center_x, center_y = board_loc[i][j]
             pygame.draw.circle(screen, (0, 0, 0), (center_x, center_y), radius)
             font = pygame.font.SysFont(font_sys, 100)
             text = font.render(f"{board[i][j]}", True, (255, 255, 255))
@@ -35,11 +33,41 @@ def update_screen():
             screen.blit(text, (center_x - len // 2, center_y - 25))
     pygame.display.update()
 
+def seed(player):
+    seeding = True
+    while seeding:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for j in range(len(board_loc[player])):
+                    x, y = board_loc[player][j]
+                    distance = math.sqrt((pos[0] - x)**2 + (pos[1] - y)**2)
+                    if distance <= radius:
+                        nb_seeds = board[player][j]
+                        board[player][j] = 0
+                        for i in range(nb_seeds):
+                            if player == 0:
+                                next_index = (6 - j + i) % 12
+                                board[rotation[next_index][0]][rotation[next_index][1]] += 1
+                            elif player == 1:
+                                next_index = (player * 6 + j + 1 + i) % 12
+                                board[rotation[next_index][0]][rotation[next_index][1]] += 1
+                        seeding = False
+
+# Game initialization
+player = 0
+turn = 0
+
 # Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    update_screen()
+    seed(player)
     turn += 1
     update_screen()
